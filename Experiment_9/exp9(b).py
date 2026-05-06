5afb80c36c37ddb653a2178b414e58d6164669e1
0x0r_999 <129256218+0x0r666@users.noreply.github.com>
2026-05-06 17:20:56 +0530

    Update exp9(b).py

--- Experiment_9/exp9(b).py
+++ Experiment_9/exp9(b).py
@@ -1,18 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Create data using Pandas
 data = {
     "Name": ["A", "B", "C", "D"],
     "Marks": [80, 90, 70, 85]
 }
 
 df = pd.DataFrame(data)
-
-# Display DataFrame
 print(df)
-
-# Plot using Matplotlib
 plt.plot(df["Name"], df["Marks"], marker='o')
 
 plt.title("Student Marks")
